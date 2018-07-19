40538d51f4b5354d76c719273061b1b424e0090e
Karen Burgos <karen@burgos.us>
2018-07-19 10:18:11 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Beat Making Labs</h1>
+        </header>
+        <NavBar/>
+        <p>Welcome to my website!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './../App.css'
+
+class NavBar extends Component {
+  render() {
+    return (
+<div class="navigation">
+<ul>
+  <li><a href="#">Learn</a></li>
+  <li><a href="#">Sell</a></li>
+  <li><a href="#">Compete</a></li>
+  <li><a href="#">Sounds</a></li>
+  <li><a href="#">Social</a></li>
+</ul>
+</div>
+  )
+  }
+}
+
+export default NavBar;
