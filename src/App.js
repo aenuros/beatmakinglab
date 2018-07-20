84269d754ddb74c619ce436608e71af9be353114
Applejuicekid <applejuicekid@gmail.com>
2018-07-20 14:14:43 -0400

    made sell page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import NavBar from './components/elements/NavBar';
 import Sell from './components/Sell';
 import './App.css'
+import {Card, CardTitle} from 'react-materialize'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <h1>Beat Making Labs</h1>
         </header>
         <NavBar/>
+        <Sell/>
         
-        <p>Welcome to my website!
-        </p>
       </div>
     );
   }
