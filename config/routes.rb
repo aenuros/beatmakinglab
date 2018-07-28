b850fef97400db3e7efa9ae1c6d140441bda7685
Brittany Davis <bmdavis723@gmail.com>
2018-07-28 16:09:08 -0400

    fixed syntax errors in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts do
-    user do
+    member do
       put "like", to: "posts#upvote"
       put "dislike", to: "posts#downvote"
+    end
+  end
+
+
 
 resources :tutorials
 
@@ -33,5 +37,6 @@ get '/test' => 'pages#test'
 get '/postlist' => 'posts#postlist'
 
 get '/myposts' => 'posts#myposts'
+
 
 end
