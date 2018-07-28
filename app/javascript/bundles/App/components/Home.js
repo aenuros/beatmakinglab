fe00c0daa78dea09ce19ece8c1a7be90a23851af
Applejuicekid <applejuicekid@gmail.com>
2018-07-28 19:21:05 -0400

    merging

--- app/javascript/bundles/App/components/Home.js
+++ app/javascript/bundles/App/components/Home.js
@@ -1,15 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-// import BackgroundImage from '../../../../assets/images/backgroundhome.jpg';
-
-class Home extends Component {
-  render() {
-    return(
-        <div class="home">
-          <p>
-            The Homepage renders, but images are HARD!!  
-          </p>
-=======
 
 import { } from 'react-materialize';
 
@@ -18,7 +7,6 @@ class Home extends Component {
       return (
         <div>
         <img src="https://i.imgur.com/Rcfy8DH.jpg"/>
->>>>>>> 67881461ca77a22cfa49eb135f18b3c6c96bb684
         </div>
       )
     }
--- app/javascript/bundles/App/components/Social.js
+++ app/javascript/bundles/App/components/Social.js
@@ -6,15 +6,10 @@ import InstagramEmbed from 'react-instagram-embed';
 class Social extends Component {
     render() {
       return (
-<<<<<<< HEAD
         <div class="social">
         <h2>Social Media</h2>
 
         <div class="twitter">
-=======
-        <div>
-        <h3>Social Media</h3>
->>>>>>> 67881461ca77a22cfa49eb135f18b3c6c96bb684
             <TwitterTimelineEmbed
 sourceType="profile"
 screenName="beatmakinglab"
