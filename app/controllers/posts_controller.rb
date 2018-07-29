5d504cb57a26be805c3ebbe1bfe8811e3fc2c83f
Karen Burgos <karen@burgos.us>
2018-07-29 18:18:36 -0400

    upvote feature with hearts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -76,13 +76,13 @@ class PostsController < ApplicationController
   def upvote
     @post = Post.find(params[:id])
     @post.upvote_by current_user
-    redirect_to posts_path
+     redirect_back fallback_location: posts_path
   end
 
   def downvote
     @post = Post.find(params[:id])
     @post.downvote_by current_user
-    redirect_to posts_path
+     redirect_back fallback_location: posts_path
   end
 
 
